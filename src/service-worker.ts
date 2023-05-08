d9d2a3be29f8ef6395017392e610183109538e3f
Hariom Ojha <2003om1711@gmail.com>
2023-05-08 13:19:04 +0530

    updated

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -83,14 +83,14 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('my-cache').then(function(cache) {
       return cache.addAll([
-        // '/assets/icons/icon-72x72.png',
-        // '/assets/icons/icon-96x96.png',
-        // '/assets/icons/icon-128x128.png',
-        // '/assets/icons/icon-144x144.png',
-        // '/assets/icons/icon-152x152.png',
-        // '/assets/icons/icon-192x192.png',
-        // '/assets/icons/icon-384x384.png',
-        // '/assets/icons/icon-512x512.png',
+        '/assets/icons/icon-72x72.png',
+        '/assets/icons/icon-96x96.png',
+        '/assets/icons/icon-128x128.png',
+        '/assets/icons/icon-144x144.png',
+        '/assets/icons/icon-152x152.png',
+        '/assets/icons/icon-192x192.png',
+        '/assets/icons/icon-384x384.png',
+        '/assets/icons/icon-512x512.png',
               '/assets/breakfast.png',
               '/assets/cake.png',
               '/assets/egg.png',
